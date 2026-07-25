2be86c9a6a666a808bb2ade80deab9756f422927
x9136 <60105060+x9136@users.noreply.github.com>
2026-07-25 09:24:14 +0500

    Drop trailing .0 from compact number formatting

--- src/script.js
+++ src/script.js
@@ -333,9 +333,9 @@ const Utils = {
    * @returns {string}
    */
   compactNumber(num) {
-      if (num >= 1000000000) return (num / 1000000000).toFixed(1) + 'B';
-      if (num >= 1000000) return (num / 1000000).toFixed(1) + 'M';
-      if (num >= 1000) return (num / 1000).toFixed(1) + 'K';
+      if (num >= 1000000000) return parseFloat((num / 1000000000).toFixed(1)) + 'B';
+      if (num >= 1000000) return parseFloat((num / 1000000).toFixed(1)) + 'M';
+      if (num >= 1000) return parseFloat((num / 1000).toFixed(1)) + 'K';
       return num.toString();
   },
 
