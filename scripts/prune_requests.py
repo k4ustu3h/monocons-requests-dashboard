1c0ebe7014b62ad2891f30c28bc14510b958a834
x9136 <60105060+x9136@users.noreply.github.com>
2026-08-27 06:14:52 +0500

    Round ROI scores to integers

--- scripts/prune_requests.py
+++ scripts/prune_requests.py
@@ -949,7 +949,7 @@ def calculate_roi_scores():
     
     changed = 0
     for app, score in scores_list:
-        new_score = round(score, 2)
+        new_score = round(score)
         if app.get('roi_score') != new_score:
             app['roi_score'] = new_score
             changed += 1
